0e3e02421b47134feb512298e59655798d6b1dc0
Olaf Tomalka <olaf@tomalka.me>
2017-12-19 15:54:29 +0100

    Added initial version of the Newsroom contract

---
+++ contracts/Newsroom.sol
@@ -0,0 +1,61 @@
+pragma solidity 0.4.18;
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+
+
+contract Newsroom is Ownable {
+  event ArticleProposed(address indexed author, uint indexed id);
+  event ArticleApproved(uint id);
+  event ArticleDenied(uint id);
+
+  uint private latestId;
+  mapping(uint => Article) private articles;
+  mapping(uint => bool) private waiting;
+  mapping(uint => bool) private approved;
+
+  function author(uint articleId) public view returns (address) {
+    return articles[articleId].author;
+  }
+
+  function uri(uint articleId) public view returns (string) {
+    return articles[articleId].uri;
+  }
+
+  function timestamp(uint articleId) public view returns (uint) {
+    return articles[articleId].timestamp;
+  }
+
+  function proposeArticle(string articleUri) public returns (uint) {
+    uint id = latestId;
+    latestId++;
+
+    articles[id] = Article(
+      articleUri,
+      msg.sender,
+      now
+    );
+    waiting[id] = true;
+    ArticleProposed(msg.sender, id);
+    return id;
+  }
+
+  function approveArticle(uint id) public onlyOwner {
+    require(waiting[id] == true);
+    require(articles[id].author != 0x0);
+    delete waiting[id];
+    approved[id] = true;
+    ArticleApproved(id);
+  }
+
+  function denyArticle(uint id) public onlyOwner {
+    require(waiting[id] == true);
+    delete waiting[id];
+    delete articles[id];
+    ArticleDenied(id);
+  }
+
+  struct Article {
+    string uri;
+    address author;
+    uint timestamp;
+  }
+}
